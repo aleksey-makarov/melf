b8292675347edd7b270c9a7b200943ea9bcff4d8
Aleksey Makarov <aleksey.makarov@gmail.com>
2021-06-29 14:15:23 +0300

    bump nixos-unstable

--- default.nix
+++ default.nix
@@ -1,7 +1,7 @@
 let
   # nixos-unstable latest
   # FIXME: use niv
-  pkgs = import (fetchTarball https://github.com/NixOS/nixpkgs/archive/64b4617883844efe0cc20163e007ee636462eb18.tar.gz) { };
+  pkgs = import (fetchTarball https://github.com/NixOS/nixpkgs/archive/e1f8852faac7638e88d5e8a5b9ee2a7568685e3f.tar.gz) { };
 
 in
   { melf = pkgs.haskellPackages.callPackage ./melf.nix { };
