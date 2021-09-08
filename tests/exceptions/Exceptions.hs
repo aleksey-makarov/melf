6f37dd9abc8a409fb4d80c2bfe748c798f578b7d
Aleksey Makarov <aleksey.makarov@gmail.com>
2021-09-08 23:00:00 +0300

    exceptions: Wip testcase

--- tests/exceptions/Exceptions.hs
+++ tests/exceptions/Exceptions.hs
@@ -1,7 +1,30 @@
+{-# LANGUAGE TemplateHaskell #-}
+
 module Main (main) where
 
 import Test.Tasty
 import Test.Tasty.HUnit
 
+import Control.Exception.ChainedException
+
+f1 :: IO ()
+f1 = $chainedError "some error @1"
+
+f2 :: IO ()
+f2 = $chainedError'
+
+f11 :: IO ()
+f11 = $addContext "some context @11" $ f1
+
+f21 :: IO ()
+f21 = $addContext' $ f1
+
+checkExceptions :: IO () -> IO ()
+checkExceptions x = x
+
 main :: IO ()
-main = defaultMain $ testCase "Ok" $ return ()
+main = defaultMain $ testGroup "exceptions" [ testCase "f1"  $ checkExceptions f1
+                                            , testCase "f2"  $ checkExceptions f2
+                                            , testCase "f11" $ checkExceptions f11
+                                            , testCase "f21" $ checkExceptions f21
+                                            ]
