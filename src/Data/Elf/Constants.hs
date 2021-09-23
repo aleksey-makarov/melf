6256fa125b1ca00eb393114ee544a4a91d49cb90
Aleksey Makarov <aleksey.makarov@gmail.com>
2021-09-23 22:29:26 +0300

    docs: Wip (Constants)

--- src/Data/Elf/Constants.hs
+++ src/Data/Elf/Constants.hs
@@ -1,5 +1,18 @@
+-- |
+-- Module      : Data.ELF.Constants
+-- Description : Definitions of constants used in ELF files
+-- Copyright   : (c) Aleksey Makarov, 2021
+-- License     : BSD 3-Clause License
+-- Maintainer  : aleksey.makarov@gmail.com
+-- Stability   : experimental
+-- Portability : portable
+--
+-- Definitions of constants used in ELF files
 
 -- https://stackoverflow.com/questions/10672981/export-template-haskell-generated-definitions
 
-module Data.Elf.Constants (module Data.Elf.Constants.Data) where
+module Data.Elf.Constants (
+    -- $docs
+    module Data.Elf.Constants.Data
+    ) where
 import Data.Elf.Constants.Data
