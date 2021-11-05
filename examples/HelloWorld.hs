d10c7486a267ccbcbcff716ccb682fee485534e6
Aleksey Makarov <aleksey.makarov@gmail.com>
2021-11-05 15:00:30 +0200

    HelloWorld.hs: Create symbols for syscalls

--- examples/HelloWorld.hs
+++ examples/HelloWorld.hs
@@ -6,11 +6,16 @@ import Prelude as P
 
 import Control.Monad.Catch
 import Control.Monad.State
+import Data.Word
 
 import AsmAarch64
 
 msg :: String
 msg = "Hello World!\n"
+
+sys_exit, sys_write :: Word16
+sys_write = 64
+sys_exit = 93
 
 helloWorld :: MonadCatch m => StateT CodeState m ()
 helloWorld = do
@@ -19,11 +24,11 @@ helloWorld = do
     mov x0 1                             --     mov x0, #1
     ascii msg >>= adr x1                 --     ldr x1, =msg
     mov x2 $ fromIntegral $ P.length msg --     ldr x2, =len
-    mov x8 64                            --     mov x8, #64 // write()
+    mov x8 sys_write                     --     mov x8, #64 // write()
     svc 0                                --     svc #0
                                          --
     mov x0 0                             --     mov x0, #0
-    mov x8 93                            --     mov x8, #93 // exit()
+    mov x8 sys_exit                      --     mov x8, #93 // exit()
     svc 0                                --     svc #0
                                          --
                                          -- .ascii "Hello World!\n"
