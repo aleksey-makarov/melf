a2b80862674db8d5fb67b21fd2bf887c5c8d8fab
Aleksey Makarov <aleksey.makarov@gmail.com>
2021-11-06 10:35:02 +0200

    HelloWorld.hs: Fix formatting

--- examples/HelloWorld.hs
+++ examples/HelloWorld.hs
@@ -21,16 +21,14 @@ helloWorld :: MonadCatch m => StateT CodeState m ()
 helloWorld = do
 
     start <- label
-    exportSymbol "_start" start          -- _start:
-    mov x0 1                             --     mov x0, #1
-    helloString <- ascii msg             --
-    adr x1 helloString                   --     ldr x1, =msg
-    mov x2 $ fromIntegral $ P.length msg --     ldr x2, =len
-    mov x8 sys_write                     --     mov x8, #64 // write()
-    svc 0                                --     svc #0
-                                         --
-    mov x0 0                             --     mov x0, #0
-    mov x8 sys_exit                      --     mov x8, #93 // exit()
-    svc 0                                --     svc #0
-                                         --
-                                         -- .ascii "Hello World!\n"
+    exportSymbol "_start" start
+    mov x0 1
+    helloString <- ascii msg
+    adr x1 helloString
+    mov x2 $ fromIntegral $ P.length msg
+    mov x8 sys_write
+    svc 0
+
+    mov x0 0
+    mov x8 sys_exit
+    svc 0
