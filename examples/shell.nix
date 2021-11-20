ab655cab27d99e828b0d06fe7fea4d49612d66e8
Aleksey Makarov <aleksey.makarov@gmail.com>
2021-11-20 23:00:00 +0300

    Use melf from hackage

--- examples/shell.nix
+++ examples/shell.nix
@@ -4,14 +4,12 @@ let
   pkgsCross = import sources.nixpkgs {
     crossSystem.config = "aarch64-unknown-linux-gnu";
   };
-  melfSrc = sources.melf;
-  melf = pkgs.haskellPackages.callCabal2nix "melf" melfSrc {};
   ghc = pkgs.haskellPackages.ghcWithPackages (ps: with ps; [
     melf
   ]);
 in
   pkgs.mkShell {
-    buildInputs = with pkgs ; [ qemu cabal-install pandoc ghc pkgsCross.buildPackages.gcc ];
+    buildInputs = with pkgs ; [ nix niv cabal-install pandoc ghc pkgsCross.buildPackages.gcc qemu ];
     # 8.10.4: en_US.UTF-8 => ascii, C.UTF-8 => utf8
     # 9.0.1:  en_US.UTF-8 => utf8,  C.UTF-8 => ascii
     # WTF?
