35ef18df8be4240fd3bde0299d9152e201cda583
Aleksey Makarov <aleksey.makarov@gmail.com>
2021-12-20 21:59:39 +0300

    examples: Use melf v1.0.1 from GitHub

--- examples/shell.nix
+++ examples/shell.nix
@@ -4,6 +4,8 @@ let
   pkgsCross = import sources.nixpkgs {
     crossSystem.config = "aarch64-unknown-linux-gnu";
   };
+  melfSrc = sources.melf;
+  melf = pkgs.haskellPackages.callCabal2nix "melf" melfSrc {};
   ghc = pkgs.haskellPackages.ghcWithPackages (ps: with ps; [
     melf
   ]);
