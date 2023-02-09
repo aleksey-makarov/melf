e77978bdf82fa1902c46eb5f3119451bfa62ce72
Aleksei Makarov <aleksei.makarov@opensynergy.com>
2023-02-09 12:50:56 +0100

    Bump nixpkgs version, fix CI

--- default.nix
+++ default.nix
@@ -1,40 +1,54 @@
 # How to call for another GHC version:
-# - nix-shell --argstr compilerVersion ghc884
+# - nix-shell --argstr compilerVersion ghc94
 # - or just uncomment below
 
-# { compilerVersion ? "ghc884" }:
+# { compilerVersion ? "ghc884"  }:
 # { compilerVersion ? "ghc8107" }:
-# { compilerVersion ? "ghc902" }:
-{ compilerVersion ? "ghc924" }:
-# { compilerVersion ? "ghc941" }:
+# { compilerVersion ? "ghc902"  }:
+  { compilerVersion ? "ghc924"  }: # default
+# { compilerVersion ? "ghc925"  }:
+# { compilerVersion ? "ghc944"  }:
 let
   sources = import ./nix/sources.nix;
   pkgs = import sources.nixpkgs { };
 
-  source-overrides-884 = {
-    singletons = "2.6";
-    th-desugar = "1.10";
-  };
-  source-overrides-8107 = {
-    singletons = "2.7";
-    th-desugar = "1.11";
-  };
-  source-overrides-902 = {};
-  source-overrides-924 = {
-    singletons-base = "3.1";
-    singletons-th = "3.1";
-  };
-  source-overrides-941 = {};
+  source-overrides = {
+    ghc884 = {
+      singletons = "2.6";
+      th-desugar = "1.10";
+    };
+    ghc8107 = {
+      singletons = "2.7";
+      th-desugar = "1.11";
+    };
+    ghc902 = {
+      th-desugar = "1.12";
+      singletons = "3.0";
+      singletons-base = "3.0";
+      singletons-th = "3.0";
+    };
+    ghc924 = {};
+    ghc925 = {};
+    ghc944 = {};
+  }."${compilerVersion}";
 
-  source-overrides =
-    if compilerVersion == "ghc884"  then source-overrides-884  else
-    if compilerVersion == "ghc8107" then source-overrides-8107 else
-    if compilerVersion == "ghc902"  then source-overrides-902  else
-    if compilerVersion == "ghc924"  then source-overrides-924  else
-    # if compilerVersion == "ghc941"  then source-overrides-941  else
-    throw "version ${compilerVersion} is not supported";
+  overrides = self: super: {
+    ghc884 = {};
+    ghc8107 = {};
+    ghc902 = {};
+    ghc924 = {};
+    ghc925 = {};
+    ghc944 = {
+      singletons = super.singletons_3_0_2;
+      singletons-base = super.singletons-base_3_1_1;
+      singletons-th = super.singletons-th_3_1_1;
+      th-desugar = super.th-desugar_1_14;
+      string-qq = pkgs.haskell.lib.doJailbreak super.string-qq;
+      chell = pkgs.haskell.lib.doJailbreak super.chell;
+    };
+  }."${compilerVersion}";
 
-  myHaskellPackages = pkgs.haskell.packages."${compilerVersion}";
+  myHaskellPackages = pkgs.haskell.packages."${compilerVersion}".override { inherit overrides; };
 
 in
   myHaskellPackages.developPackage {
@@ -43,11 +57,11 @@ in
       pkgs.haskell.lib.addBuildTools drv (
         with myHaskellPackages;
         [ cabal-install
-          cabal-plan
           cabal2nix
           hpack
           niv
         ]
       );
     inherit source-overrides;
+    # FIXME: overrides do not work here
   }
