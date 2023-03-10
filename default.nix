de28edc43ea1b6c8d5737592391a388b5a1ea36a
Aleksei Makarov <aleksey.makarov@gmail.com>
2023-03-10 22:16:29 +0100

    Fix default.nix

--- default.nix
+++ default.nix
@@ -12,43 +12,7 @@ let
   sources = import ./nix/sources.nix;
   pkgs = import sources.nixpkgs { };
 
-  source-overrides = {
-    ghc884 = {
-      singletons = "2.6";
-      th-desugar = "1.10";
-    };
-    ghc8107 = {
-      singletons = "2.7";
-      th-desugar = "1.11";
-    };
-    ghc902 = {
-      th-desugar = "1.12";
-      singletons = "3.0";
-      singletons-base = "3.0";
-      singletons-th = "3.0";
-    };
-    ghc924 = {};
-    ghc925 = {};
-    ghc944 = {};
-  }."${compilerVersion}";
-
-  overrides = self: super: {
-    ghc884 = {};
-    ghc8107 = {};
-    ghc902 = {};
-    ghc924 = {};
-    ghc925 = {};
-    ghc944 = {
-      singletons = super.singletons_3_0_2;
-      singletons-base = super.singletons-base_3_1_1;
-      singletons-th = super.singletons-th_3_1_1;
-      th-desugar = super.th-desugar_1_14;
-      string-qq = pkgs.haskell.lib.doJailbreak super.string-qq;
-      chell = pkgs.haskell.lib.doJailbreak super.chell;
-    };
-  }."${compilerVersion}";
-
-  myHaskellPackages = pkgs.haskell.packages."${compilerVersion}".override { inherit overrides; };
+  myHaskellPackages = pkgs.haskell.packages."${compilerVersion}";
 
 in
   myHaskellPackages.developPackage {
@@ -59,9 +23,6 @@ in
         [ cabal-install
           cabal2nix
           hpack
-          niv
         ]
       );
-    inherit source-overrides;
-    # FIXME: overrides do not work here
   }
