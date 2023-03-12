932e34833e44aafc4367c1aef934c7fb210020dc
Aleksey Makarov <aleksey.makarov@gmail.com>
2023-03-12 21:32:21 +0100

    Data.Elf.Constants.TH: Support docs for generated code

--- src/Data/Elf/Constants.hs
+++ src/Data/Elf/Constants.hs
@@ -12,7 +12,6 @@
 -- https://stackoverflow.com/questions/10672981/export-template-haskell-generated-definitions
 
 module Data.Elf.Constants (
-    -- $docs
     module Data.Elf.Constants.Data
     ) where
 import Data.Elf.Constants.Data
